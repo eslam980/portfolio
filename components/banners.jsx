034543dda552d494a43f6ca68aff6320d0fba787
eslam980 <50788147+eslam980@users.noreply.github.com>
2022-12-17 00:20:29 +0200

    ha

--- components/banners.jsx
+++ components/banners.jsx
@@ -11,23 +11,23 @@ const Banners = () => {
 
             <div className='bg-zinc-900 py-6 rounded-md shadow-md shadow-black grid md:grid-cols-5 gap-8'>
 
-                <a href="/crypto" className='mx-6  flex  items-center justify-center py-5'>
+                <a href="/goggeri" className='mx-6  flex  items-center justify-center py-5'>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/1/18/Fiverr_Logo_09.2020.svg" height='200' width='200' alt="image"/>
                 </a>
 
-                <a href="/crypto" className=' mx-6  flex  items-center justify-center py-5'>
+                <a href="/goggeri" className=' mx-6  flex  items-center justify-center py-5'>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Upwork-logo.svg" height='200' width='200' alt="image"/>
                 </a>
 
-                <a href="/crypto" className='mx-6  flex  items-center justify-center py-5'>
+                <a href="/goggeri" className='mx-6  flex  items-center justify-center py-5'>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/8/8f/Kickstarter_logo_2019.svg" height='200' width='200' alt="image"/>
                 </a>
 
-                <a href="/crypto" className='mx-6  flex  items-center justify-center py-5'>
+                <a href="/goggeri" className='mx-6  flex  items-center justify-center py-5'>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Unity_2021.svg" height='200' width='200' alt="image"/>
                 </a>
 
-                <a href="/crypto" className='mx-6  flex  items-center justify-center py-5'>
+                <a href="/goggeri" className='mx-6  flex  items-center justify-center py-5'>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png" height='200' width='200' alt="image"/>
                 </a>
 
